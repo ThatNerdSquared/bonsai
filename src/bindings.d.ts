c5a44ace11c2f5a77bb9e7996b83c7e6be820ffa
ThatNerdSquared <72814106+ThatNerdSquared@users.noreply.github.com>
2022-07-04 06:20:32 -0700

    feat: fix TS errors

---
+++ src/bindings.d.ts
@@ -0,0 +1,9 @@
+export {};
+
+declare global {
+    const APPLICATION_ID: string;
+    const APPLICATION_SECRET: string;
+    const PUBLIC_KEY: string;
+    const DB_URL: string;
+    const DB_KEY: string;
+}
