a8e33b1b437e149448432ce1df94a0b49d2dc5d3
ThatNerdSquared <72814106+ThatNerdSquared@users.noreply.github.com>
2022-09-25 15:50:38 -0700

    feat: burn it to the ground

--- src/bindings.d.ts
+++
@@ -1,9 +0,0 @@
-export {}
-
-declare global {
-    const APPLICATION_ID: string
-    const APPLICATION_SECRET: string
-    const PUBLIC_KEY: string
-    const DB_URL: string
-    const DB_KEY: string
-}
